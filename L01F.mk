555abb0e96f9674748798e3316927ca1cae080da
scoty755 <scoty755@gmail.com>
2014-03-01 22:27:03 +0900

    cm-10.2 L01F

--- L01F.mk
+++ L01F.mk
@@ -18,17 +18,17 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES := device/lge/L01F/overlay/packages/providers/TelephonyProvider/res/config.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-PRODUCT_DEVICE := d802
-PRODUCT_NAME := full_d802
+PRODUCT_DEVICE := L01F
+PRODUCT_NAME := full_L01F
 PRODUCT_BRAND := LGE
-PRODUCT_MODEL := LG-D802
+PRODUCT_MODEL := LG-L01F
 PRODUCT_MANUFACTURER := lge
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/d802/device.mk)
+$(call inherit-product, device/lge/L01F/device.mk)
