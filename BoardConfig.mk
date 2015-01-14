e2bf7434a0f2e7500bbf241a066fe51f70120109
sudosurootdev <sudosurootdev@gmail.com>
2015-01-14 11:12:38 +0000

    Fixup LG L22 variant for building and use.
    Change-Id: Iff9ff5e9c1f837ef9e14cfa20286e183ce589a2b
    Signed-off-by: sudosurootdev <sudosurootdev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,13 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 TARGET_RECOVERY_FSTAB := device/lge/lgl22/rootdir/recovery.fstab
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_lgl22_defconfig
+TARGET_KERNEL_CONFIG := g2-kddi-perf_defconfig
 
-TARGET_OTA_ASSERT_DEVICE := lgl22,g2,galbi
+TARGET_OTA_ASSERT_DEVICE := lgl22,l22,g2,galbi
 
 G2_DTS_TARGET := msm8974-g2-kddi
 
